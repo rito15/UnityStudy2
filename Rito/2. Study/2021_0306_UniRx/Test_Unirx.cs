f2e99c5f8c1197823e69eea96d6ddf0bec6e4f93
rito15 <ghdtkals15@gmail.com>
2021-03-16 04:02:59 +0900

    [2021-03-16] update

--- Rito/2. Study/2021_0306_UniRx/Test_Unirx.cs
+++ Rito/2. Study/2021_0306_UniRx/Test_Unirx.cs
@@ -21,35 +21,89 @@ public class Test_Unirx : MonoBehaviour
     public Button _targetButton2;
 
     [Range(0, 1)]
-    public float _value;
+    public float _intValue;
+
+    public bool _boolValue;
+
+    public IntReactiveProperty _intProp = new IntReactiveProperty(0);
+    public ReactiveProperty<int> _intProp2 = new ReactiveProperty<int>(1);
 
     private void Start()
     {
         //EventAsStream();
         //DragAndDrop();
         //LifeSpan();
-        CheckDoubleClickImmediatley();
 
+        //CheckDoubleClickImmediatley();
         //CheckDoubleKeyPress(KeyCode.A, () => transform.Translate(Vector3.left * Time.deltaTime * 5f));
         //CheckDoubleKeyPress(KeyCode.D, () => transform.Translate(Vector3.right * Time.deltaTime * 5f));
-        CheckDoubleKeyPressImmediatley(KeyCode.W, () => Debug.Log("WW"));
+        //CheckDoubleKeyPressImmediatley(KeyCode.W, () => Debug.Log("WW"));
 
         //ObserveValueChanged();
+        //AsyncGetFromWeb();
+
+        //RotateObjectWithDrag();
+
+        //UpdateDifferences();
+        //CaptureWhenValueChanges();
+        //RefineRapidlyChangingValue();
+
     }
 
-    // T o D o : 그냥 변수의 값 변화 인식
-
-    private void Update()
+    private void UpdateDifferences()
     {
+        // 게임오브젝트가 활성화된 동안에만 OnNext() 통지
+        // 게임오브젝트가 파괴될 때 OnCompleted()
+        this.UpdateAsObservable()
+            .Where(_ => Input.GetMouseButtonDown(0))
+            .Subscribe(
+                _ => Debug.Log("UpdateAsObservable" + this._intValue),
+                () => Debug.Log("UpdateAsObservable Completed")
+            );
 
+        // 대상 객체가 활성화된 동안에만 OnNext() 통지
+        // 대상 객체가 파괴될 때 OnCompleted()
+        this.ObserveEveryValueChanged(_ => Input.GetMouseButton(0))
+            .Where(x => x)
+            .Skip(TimeSpan.Zero)
+            .Subscribe(
+                _ => Debug.Log("ObserveEveryValueChanged"), 
+                () => Debug.Log("ObserveEveryValueChanged Completed")
+            );
 
+        // 독자적
+        Observable.EveryUpdate()
+            .Where(_ => Input.GetMouseButtonDown(1))
+            .Subscribe(
+                _ => Debug.Log("EveryUpdate" + this._intValue),
+                () => Debug.Log("EveryUpdate Completed")
+            );
+    }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+    private void AsyncGetFromWeb()
+    {
+        // Obsolete : Use UnityEngine.Networking.UnityWebRequest Instead.
+        ObservableWWW.Get("http://google.co.kr/")
+            .Subscribe(
+                x => Debug.Log(x.Substring(0, 20)), // onSuccess
+                ex => Debug.LogException(ex)       // onError
+            );
+    }
+
+    private void WhenAllExample()
+    {
+        var parallel = Observable.WhenAll(
+            ObservableWWW.Get("http://google.com/"),
+            ObservableWWW.Get("http://bing.com/"),
+            ObservableWWW.Get("http://unity3d.com/")
+        );
+
+        parallel.Subscribe(xs =>
         {
-            Debug.Log(_targetButton == null, ReferenceEquals(_targetButton, null));
-            if(_targetButton == null)
-                _targetButton = null;
-        }
+            Debug.Log(xs[0].Substring(0, 100)); // google
+            Debug.Log(xs[1].Substring(0, 100)); // bing
+            Debug.Log(xs[2].Substring(0, 100)); // unity
+        });
     }
 
     private void CheckDoubleClick()
@@ -160,6 +214,31 @@ public class Test_Unirx : MonoBehaviour
             .Subscribe(x => Debug.Log(x));
     }
 
+    private void RotateObjectWithDrag()
+    {
+        float rotSpeed = 500f;
+
+        // 오브젝트(콜라이더)에 마우스 클릭하여 드래그하면 마우스 이동거리에 따라 회전
+        // 마우스 떼면 스트림 종료 및 다시 시작(Repeat)
+
+        this.UpdateAsObservable()
+            .SkipUntil(this.OnMouseDownAsObservable())
+            .Select(_ =>
+                new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"))
+            )
+            .TakeUntil(this.OnMouseUpAsObservable())
+            .RepeatUntilDisable(this)
+            .Subscribe(move =>
+            {
+                transform.rotation = 
+                    Quaternion.AngleAxis(move.y * rotSpeed * Time.deltaTime, Vector3.right) *
+                    Quaternion.AngleAxis(-move.x * rotSpeed * Time.deltaTime, Vector3.up) *
+                    transform.rotation;
+            });
+
+
+
+    }
     private void LifeSpan()
     {
         Observable.Timer(TimeSpan.FromSeconds(3.0))
@@ -167,10 +246,82 @@ public class Test_Unirx : MonoBehaviour
             .Subscribe(_ => Destroy(gameObject));
     }
 
-    private void ObserveValueChanged()
+    // 값이 변하는 순간을 포착
+    private void CaptureWhenValueChanges()
+    {
+        // 마우스 클릭, 떼는 순간 모두 포착
+        this.UpdateAsObservable()
+            .Select(_ => Input.GetMouseButton(0))
+            .DistinctUntilChanged()
+            .Skip(1) // 시작하자마자 false값에 대한 판정 때문에 "Up" 호출되는 것 방지
+            .Subscribe(down =>
+            {
+                if (down)
+                    Debug.Log($"Down : {Time.frameCount}");
+                else
+                    Debug.Log($"Up : {Time.frameCount}");
+            });
+
+
+        // 값이 false -> true로 바뀌는 순간만 포착
+        this.UpdateAsObservable()
+            .Select(_ => this._boolValue)
+            .DistinctUntilChanged()
+            .Where(x => x)
+            .Skip(TimeSpan.Zero) // 초기값이 true일 때 첫 프레임에 바로 호출되는 것 방지
+            .Subscribe(_ => Debug.Log("TRUE"));
+
+        // 매 프레임, 값의 변화 포착만을 위한 간단한 구문 (위 구문을 간소화)
+        this.ObserveEveryValueChanged(_ => this._boolValue)
+            .Where(x => x)
+            .Skip(TimeSpan.Zero)
+            .Subscribe(_ => Debug.Log("TRUE 2"));
+    }
+
+    private void TestObserveValueChanged()
     {
         // ObserveEveryValueChanged : 클래스 타입에 대해 모두 사용 가능
-        this.ObserveEveryValueChanged(x => x._value)
+        this.ObserveEveryValueChanged(x => x._intValue)
             .Subscribe(x => Debug.Log("Value Changed : " + x));
+    }
+
+    private void TestObserveValueOnEveryUpdate()
+    {
+        // 대상의 값을 그냥 매 프레임 조건 없이 출력
+        this.UpdateAsObservable()
+            .Select(_ => this._intValue)
+            .Subscribe(x => Debug.Log(x));
+    }
+
+
+    public bool _isTouched = false;
+    public bool _isTouchedRefined = false;
+    private bool _isGroundedRefined = false;
+    private void OnTriggerEnter(Collider other) => _isTouched = true;
+    private void OnTriggerExit(Collider other) => _isTouched = false;
+
+    // 급변하는 값 정제하기
+    private void RefineRapidlyChangingValue()
+    {
+        // ObserveEveryValueChanged : 값이 변화했을 때 통지
+        // ThrottleFrame(5) : 마지막 통지로부터 5프레임동안 값의 통지를 받지 않으면 OnNext()
+
+        // 따라서 값이 급변하는 동안에는 OnNext() 하지 않고
+        // 5프레임 이내로 값이 변하지 않았을 때 마지막으로 기억하는 값을 전달
+
+        // 5프레임 이내에서 순간적으로 급변하는 값들을 무시하여, 값을 정제하는 효과가 있음
+
+        // 사용 예시 : 닿았는지 여부 검사, 비탈길에서의 isGrounded 검사
+
+        this.ObserveEveryValueChanged(_ => this._isTouched)
+            .ThrottleFrame(5)
+            .Subscribe(x => _isTouchedRefined = x);
+
+        TryGetComponent(out CharacterController cc);
+        cc.UpdateAsObservable()
+            .Select(_ => cc.isGrounded)
+            .DistinctUntilChanged()
+            .ThrottleFrame(5)
+            .Subscribe(x => _isGroundedRefined = x);
     }
 }

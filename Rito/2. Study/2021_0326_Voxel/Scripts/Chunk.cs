b016635b09397f453f0ab095140987b4089aabcd
rito15 <ghdtkals15@gmail.com>
2021-03-27 21:51:25 +0900

    [2021-03-27] Voxel System - 2

--- Rito/2. Study/2021_0326_Voxel/Scripts/Chunk.cs
+++ Rito/2. Study/2021_0326_Voxel/Scripts/Chunk.cs
@@ -11,33 +11,127 @@ namespace Rito.VoxelSystem
     /// <summary> 메시 청크 컴포넌트 </summary>
     public class Chunk : MonoBehaviour
     {
+        /***********************************************************************
+        *                               Public Fields
+        ***********************************************************************/
+        #region .
         public MeshRenderer meshRenderer;
         public MeshFilter meshFilter;
 
+        #endregion
+        /***********************************************************************
+        *                               Private Fields
+        ***********************************************************************/
+        #region .
+        private int vertexIndex = 0;
+        private List<Vector3> vertices = new List<Vector3>();
+        private List<int> triangles = new List<int>();
+        private List<Vector2> uvs = new List<Vector2>();
+
+        /// <summary> Solid 여부(해당 좌표에 복셀 큐브가 존재해야 하는지 여부(true/false))를 갖는 맵 데이터 </summary>
+        private bool [,,] voxelMap = new bool[VoxelData.ChunkWidth, VoxelData.ChunkHeight, VoxelData.ChunkWidth];
+
+        #endregion
+        /***********************************************************************
+        *                               Unity Events
+        ***********************************************************************/
+        #region .
         private void Start()
         {
-            int vertexIndex = 0;
-            List<Vector3> vertices = new List<Vector3>();
-            List<int> triangles = new List<int>();
-            List<Vector2> uvs = new List<Vector2>();
+            PopulateVoxelMap();
+            CreateMeshData();
+            CreateMesh();
+        }
 
-            // 6방향의 면 그리기
-            for (int p = 0; p < 6; p++)
+        #endregion
+        /***********************************************************************
+        *                               Private Methods
+        ***********************************************************************/
+        #region .
+        /// <summary> 복셀 맵의 Solid 정보 데이터 생성 </summary>
+        private void PopulateVoxelMap()
+        {
+            for (int y = 0; y < VoxelData.ChunkHeight; y++)
             {
-                // 각 면의 삼각형 2개 그리기
-                for (int i = 0; i < 6; i++)
+                for (int x = 0; x < VoxelData.ChunkWidth; x++)
                 {
-                    int triangleIndex = VoxelData.voxelTris[p, i];
-
-                    vertices.Add(VoxelData.voxelVerts[triangleIndex]);
-                    triangles.Add(vertexIndex);
-                    uvs.Add(VoxelData.voxelUvs[i]);
-
-                    vertexIndex++;
+                    for (int z = 0; z < VoxelData.ChunkWidth; z++)
+                    {
+                        voxelMap[x, y, z] = true;
+                    }
                 }
             }
+        }
 
-            // 메시에 데이터들 초기화
+        /// <summary> 복셀 면 그려내기 </summary>
+        private void CreateMeshData()
+        {
+            for (int y = 0; y < VoxelData.ChunkHeight; y++)
+            {
+                for (int x = 0; x < VoxelData.ChunkWidth; x++)
+                {
+                    for (int z = 0; z < VoxelData.ChunkWidth; z++)
+                    {
+                        AddVoxelDataToChunk(new Vector3(x, y, z));
+                    }
+                }
+            }
+        }
+
+        /// <summary> 해당 좌표가 Solid인지 여부 검사(해당 좌표에 복셀 큐브가 존재하는지 여부) </summary>
+        private bool CheckVoxel(Vector3 pos)
+        {
+            int x = Mathf.FloorToInt(pos.x);
+            int y = Mathf.FloorToInt(pos.y);
+            int z = Mathf.FloorToInt(pos.z);
+
+            // 맵 범위를 벗어나는 경우
+            if(x < 0 || x > VoxelData.ChunkWidth - 1 || 
+               y < 0 || y > VoxelData.ChunkHeight - 1 || 
+               z < 0 || z > VoxelData.ChunkWidth - 1)
+                return false;
+
+            return voxelMap[x, y, z];
+        }
+
+        /// <summary> 해당 좌표에서 복셀 큐브의 메시 데이터 추가 </summary>
+        private void AddVoxelDataToChunk(Vector3 pos)
+        {
+            // 6방향의 면 그리기
+            // p : -Z, +Z, +Y, -Y, -X, +X 순서로 이루어진, 큐브의 각 면에 대한 인덱스
+            for (int p = 0; p < 6; p++)
+            {
+                // Face Check(면이 바라보는 방향으로 +1 이동하여 확인)를 했을 때 
+                // Solid가 아닌 경우에만 큐브의 면이 그려지도록 하기
+                // => 청크의 외곽 부분만 면이 그려지고, 내부에는 면이 그려지지 않도록
+                if (CheckVoxel(pos) && !CheckVoxel(pos + VoxelData.faceChecks[p]))
+                {
+                    // 각 면(삼각형 2개) 그리기
+
+                    // 1. Vertex, UV 4개 추가
+                    for (int i = 0; i <= 3; i++)
+                    {
+                        vertices.Add(VoxelData.voxelVerts[VoxelData.voxelTris[p, i]] + pos);
+                        uvs.Add(VoxelData.voxelUvs[i]);
+                    }
+
+                    // 2. Triangle의 버텍스 인덱스 6개 추가
+                    triangles.Add(vertexIndex);
+                    triangles.Add(vertexIndex + 1);
+                    triangles.Add(vertexIndex + 2);
+
+                    triangles.Add(vertexIndex + 2);
+                    triangles.Add(vertexIndex + 1);
+                    triangles.Add(vertexIndex + 3);
+
+                    vertexIndex += 4;
+                }
+            }
+        }
+
+        /// <summary> vertex, triangle, uv 데이터 이용해 메시 생성  </summary>
+        private void CreateMesh()
+        {
             Mesh mesh = new Mesh
             {
                 vertices = vertices.ToArray(),
@@ -49,5 +143,7 @@ namespace Rito.VoxelSystem
 
             meshFilter.mesh = mesh;
         }
+
+        #endregion
     }
 }
--- Rito/2. Study/2021_0326_Voxel/Scripts/Static/VoxelData.cs
+++ Rito/2. Study/2021_0326_Voxel/Scripts/Static/VoxelData.cs
@@ -10,6 +10,9 @@ namespace Rito.VoxelSystem
 {
     public static class VoxelData
     {
+        public static readonly int ChunkWidth = 5;
+        public static readonly int ChunkHeight = 5;
+
         /***********************************************************************
         *                               Lookup Tables
         ***********************************************************************/
@@ -27,49 +30,64 @@ namespace Rito.VoxelSystem
         /// <summary> 큐브의 8개 버텍스의 상대 위치 </summary>
         public static readonly Vector3[] voxelVerts = new Vector3[8]
         {
-            // Front
+            // Back (-Z)
             new Vector3(0.0f, 0.0f, 0.0f), // LB
             new Vector3(1.0f, 0.0f, 0.0f), // RB
             new Vector3(1.0f, 1.0f, 0.0f), // RT
             new Vector3(0.0f, 1.0f, 0.0f), // LT
 
-            // Back
+            // Front (+Z)
             new Vector3(0.0f, 0.0f, 1.0f), // LB
             new Vector3(1.0f, 0.0f, 1.0f), // RB
             new Vector3(1.0f, 1.0f, 1.0f), // RT
             new Vector3(0.0f, 1.0f, 1.0f), // LT
         };
 
-        // 한 면을 이루는 삼각형은 2개
-        // 버텍스 인덱스는 시계방향으로 배치(전면으로 그려지도록)
-        // 각 면의 버텍스 순서는 해당 면을 기준으로 LB-LT-RB, RB-LT-RT
-        /*
+        /// <summary> 면이 바라보고 있는 방향의 방향벡터 값 </summary>
+        public static readonly Vector3[] faceChecks = new Vector3[6]
+        {
+            new Vector3( 0.0f,  0.0f, -1.0f), // Back Face   (-Z)
+            new Vector3( 0.0f,  0.0f, +1.0f), // Front Face  (+Z)
+            new Vector3( 0.0f, +1.0f,  0.0f), // Top Face    (+Y)
+            new Vector3( 0.0f, -1.0f,  0.0f), // Bottom Face (-Y)
+            new Vector3(-1.0f,  0.0f,  0.0f), // Left Face   (-X)
+            new Vector3(+1.0f,  0.0f,  0.0f), // RIght Face  (+X)
+        };
+
+        /*  한 면을 이루는 삼각형은 2개
+            버텍스 인덱스는 시계방향으로 배치(전면으로 그려지도록)
+            각 면의 버텍스 순서는 해당 면을 기준으로 LB-LT-RB, RB-LT-RT
+        
             LB-LT-RB   RB-LT-RT
 
             1          1 ㅡ 2
             | ＼         ＼ |
             0 ㅡ 2          0
+
+            * 원래는 6개의 정점 인덱스가 차례대로 나열되었지만,
+            * 2개의 버텍스가 중복되므로 중복 제거하여 메모리 절약
+
+            * 각 면의 정점 4개 순서는 {LB, LT, RB, RT}
+            * 실제 한 면을 조립할 때는 0-1-2, 2-1-3 으로 사용
         */
         /// <summary> 큐브의 각 면을 이루는 삼각형들의 버텍스 인덱스 데이터 </summary>
-        public static readonly int[,] voxelTris = new int[6, 6]
+        public static readonly int[,] voxelTris = new int[6, 4]
         {
-            {0, 3, 1, 1, 3, 2 }, // Back Face   (-Z)
-            {5, 6, 4, 4, 6, 7 }, // Front Face  (+Z)
-            {3, 7, 2, 2, 7, 6 }, // Top Face    (+Y)
-            {1, 5, 0, 0, 5, 4 }, // Bottom Face (-Y)
-            {4, 7, 0, 0, 7, 3 }, // Left Face   (-X)
-            {1, 2, 5, 5, 2, 6 }, // RIght Face  (+X)
+            {0, 3, 1, 2 }, // Back Face   (-Z)
+            {5, 6, 4, 7 }, // Front Face  (+Z)
+            {3, 7, 2, 6 }, // Top Face    (+Y)
+            {1, 5, 0, 4 }, // Bottom Face (-Y)
+            {4, 7, 0, 3 }, // Left Face   (-X)
+            {1, 2, 5, 6 }, // RIght Face  (+X)
         };
 
-        /// <summary> voxelTris의 버텍스 인덱스 순서에 따라 정의된 UV 좌표 데이터 </summary>
-        public static readonly Vector2[] voxelUvs = new Vector2[6]
+        // UV의 정점 순서 역시 LB-LT-RB-RT로 voxelTris와 일치
+        /// <summary> 정점 인덱스 순서에 해당하는 UV 좌표 데이터 </summary>
+        public static readonly Vector2[] voxelUvs = new Vector2[4]
         {
             new Vector2(0.0f, 0.0f), // LB
             new Vector2(0.0f, 1.0f), // LT
             new Vector2(1.0f, 0.0f), // RB
-
-            new Vector2(1.0f, 0.0f), // RB
-            new Vector2(0.0f, 1.0f), // LT
             new Vector2(1.0f, 1.0f), // RT
         };
 

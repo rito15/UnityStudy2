316e4f0ca37a2fc4a24fa378b4e1e54436bb534d
rito15 <ghdtkals15@naver.com>
2021-08-17 04:39:36 +0900

    [2021-08-17] Playmode Save Sample

---
+++ Rito/1. Test/2021_0817_Play Mode Save Sample/Sample_PlayModeSave.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+// 날짜 : 2021-08-17 AM 3:52:51
+// 작성자 : Rito
+
+/// <summary> 
+/// 
+/// </summary>
+public class Sample_PlayModeSave : MonoBehaviour
+{
+    public float a;
+
+    /***********************************************************************
+    *                           Save Play Mode Changes
+    ***********************************************************************/
+    #region .
+#if UNITY_EDITOR
+    private class Inner_PlayModeSave
+    {
+        private static UnityEditor.SerializedObject[] targetSoArr;
+
+        [UnityEditor.InitializeOnLoadMethod]
+        private static void Run()
+        {
+            UnityEditor.EditorApplication.playModeStateChanged += state =>
+            {
+                switch (state)
+                {
+                    case UnityEditor.PlayModeStateChange.ExitingPlayMode:
+                        var targets = FindObjectsOfType(typeof(Inner_PlayModeSave).DeclaringType);
+                        targetSoArr = new UnityEditor.SerializedObject[targets.Length];
+                        for (int i = 0; i < targets.Length; i++)
+                            targetSoArr[i] = new UnityEditor.SerializedObject(targets[i]);
+                        break;
+
+                    case UnityEditor.PlayModeStateChange.EnteredEditMode:
+                        foreach (var oldSO in targetSoArr)
+                        {
+                            if (oldSO.targetObject == null) continue;
+                            var oldIter = oldSO.GetIterator();
+                            var newSO = new UnityEditor.SerializedObject(oldSO.targetObject);
+                            while (oldIter.NextVisible(true))
+                                newSO.CopyFromSerializedProperty(oldIter);
+                            newSO.ApplyModifiedProperties();
+                        }
+                        break;
+                }
+            };
+        }
+    }
+#endif
+    #endregion
+}
